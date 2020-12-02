52e4fd6af6747a9159254863de71afd90ae02d8f
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-01 19:21:48 -0500

    Test implementation
    Implementing test via https://github.com/davidbrusius/advent-of-code-elixir-starter/blob/master/test/advent_of_code/day_01_test.exs. A lot of my issue is learning this template.

--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,9 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = [1721, 979, 366, 299, 675, 1456]
 
-    assert result
-  end
-
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    assert 514579 = part1(input)
   end
 end
