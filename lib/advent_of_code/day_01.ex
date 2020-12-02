7201a8e6745ffa4a50bb562b9494b33b26fa53c3
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-01 19:23:44 -0500

    Part 1 implementation
    Taken as for loop pattern matching, combining a few techniques from https://elixirforum.com/t/advent-of-code-2020-day-1/35917/18. I like the output produced here to more deeply debug. Now that I know what I'm doing a little better, I can get to part 2 and further myself.

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,18 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  @number_to_check 2020
+
+  def part1(input) do
+    # input |> IO.inspect(label: "Input")
+    [{a, b} | _input] = for number_1 <- input, number_2 <- input,
+              number_1 + number_2 == @number_to_check,
+              do: {number_1, number_2}
+
+    IO.puts "\nThe sum of two numbers of the list which is equal to #{@number_to_check} is: \{ #{a}, #{b} \}."
+    IO.puts "The multiplication: (#{a} x #{b}) = #{a * b}."
+
+    a * b
   end
 
-  def part2(args) do
+  def part2(_input) do
   end
 end
