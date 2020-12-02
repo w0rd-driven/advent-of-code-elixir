95755eab168e4d210731be74b219e70d4c5486f2
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-01 19:19:48 -0500

    File reading
    I'm having a hard time so to kick this off, I'm piecing together what I need. First we read the file via https://github.com/patrickcarver/advent-of-code-2019-elixir/blob/master/day01/lib/day01.ex#L42. This loads the file contents into a list `[num, num, ...]`.

--- lib/mix/tasks/d01.p1.ex
+++ lib/mix/tasks/d01.p1.ex
@@ -14,4 +14,12 @@ defmodule Mix.Tasks.D01.P1 do
         |> part1()
         |> IO.inspect(label: "Part 1 Results")
   end
+
+  def get_input do
+    "../../data/day_01.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
+    |> Stream.map(&String.to_integer/1)
+  end
 end
