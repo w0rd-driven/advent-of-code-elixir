dfffe9c133e316493fd9193e07c5dc4ed5a9e9e8
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-04 11:34:57 -0500

    Day 4 part 1 parsing fields into key value pairs
    Setup our valid fields and parsing down to the key value pairs. Here's where we check to make sure the key is in the list of valid keys

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -13,8 +13,25 @@ defmodule AdventOfCode.Day04 do
   end
 
   defp is_passport_valid(passport) do
-    passport |> IO.inspect(label: "Passport")
+    # passport |> IO.inspect(label: "Passport")
     true
+    valid_keys = [
+      "byr",
+      "iyr",
+      "eyr",
+      "hgt",
+      "hcl",
+      "ecl",
+      "pid",
+      # "cid", # We don't care to include this based on the puzzle gotcha
+    ]
+    fields = passport |> String.replace("\n", " ") |> String.split(" ") # Single new lines need to be converted to spaces
+    for field <- fields do
+      field |> IO.inspect(label: "Field")
+      [key, value] = field |> String.split(":") # Oh snaps I'm learning
+      key |> IO.inspect(label: "Key")
+      value |> IO.inspect(label: "Value")
+    end
   end
 
   def part2(_input) do
