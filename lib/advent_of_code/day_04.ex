8c47a4c5f602f74756fa8cdba608488266cf7821
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-04 11:52:29 -0500

    Day 4 part 1 completion
    Modified the input to end with 2 new lines, otherwise the last passport is always skipped via this method. I can't tell if this is against the rules but this is what I would do personally more often than not. Commented out all of our debugging lines. Using `key in valid_keys` syntax to build a list of booleans that Enum.all verifies are all true before kicking it upstream to count only the valid passports.
    
    I feel like I'm kind of producing side effects or I just don't know what that phrase means. I'm getting the final answer but I can't tell if this is a good usage pattern or not. The good news of today is this is all me, no hints other than Google figuring out how to chunk in Elixir or random topics to help me push through it.

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -2,19 +2,19 @@ defmodule AdventOfCode.Day04 do
 
   def part1(input) do
     passports = input |> parse_passports
-    passports |> IO.inspect(label: "Passports")
+    # passports |> IO.inspect(label: "Passports")
   end
 
   defp parse_passports(input) do
-    count = for passport <- input do
+    passports_checked = for passport <- input do
       passport |> is_passport_valid()
     end
-    count |> Enum.count()
+    # passports_checked |> IO.inspect(label: "Passports Checked")
+    passports_checked |> Enum.count(fn x -> x == true end)
   end
 
   defp is_passport_valid(passport) do
     # passport |> IO.inspect(label: "Passport")
-    true
     valid_keys = [
       "byr",
       "iyr",
@@ -26,12 +26,14 @@ defmodule AdventOfCode.Day04 do
       # "cid", # We don't care to include this based on the puzzle gotcha
     ]
     fields = passport |> String.replace("\n", " ") |> String.split(" ") # Single new lines need to be converted to spaces
-    for field <- fields do
-      field |> IO.inspect(label: "Field")
-      [key, value] = field |> String.split(":") # Oh snaps I'm learning
-      key |> IO.inspect(label: "Key")
-      value |> IO.inspect(label: "Value")
+    check = for field <- fields do
+      # field |> IO.inspect(label: "Field")
+      [key, _value] = field |> String.split(":") # Oh snaps I'm learning
+      # key |> IO.inspect(label: "Key")
+      # value |> IO.inspect(label: "Value")
+      key in valid_keys
     end
+    check |> Enum.all?()
   end
 
   def part2(_input) do
