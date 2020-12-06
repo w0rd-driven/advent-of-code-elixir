3affdb9d0577e1f7f7f0a0fff951571986226438
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-06 12:55:30 -0500

    Day 5 part 1 test complete
    Pulled implementation largely from https://github.com/Maximization/adventofcode2020/blob/main/day5/index.js though I should've committed what I had first.
    
    I had naively worked out that it would be a binary sort and couldn't do the math with no help. I had worked out how to do a case statement but it could be greatly simplified. It wasn't insignificant work to turn that implementation into the elixir equivalent.

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,60 @@
 defmodule AdventOfCode.Day05 do
-  def part1(args) do
+  def part1(input) do
+    _seats = input |> parse_seats
   end
 
-  def part2(args) do
+  defp parse_seats(seats) do
+    seats |> IO.inspect(label: "Seats")
+    count = for seat <- seats do
+      {row, column} = seat |> parse_seat
+
+      row |> IO.inspect(label: "Row")
+      column |> IO.inspect(label: "Column")
+
+      row * 8 + column
+    end
+    count |> Enum.max()
+  end
+
+  defp parse_seat(seat) do
+    # [rows, columns] = seat |> String.split_at(7)
+    {rows, columns} = seat |> String.split_at(7)
+
+    # rows |> IO.inspect(label: "Rows")
+    # columns |> IO.inspect(label: "Columns")
+
+    {rows |> String.graphemes() |> parse_row(), columns |> String.graphemes() |> parse_column()}
+  end
+
+  defp new_range(range, keep_lower) do
+    [lower, upper] = range
+    # lower |> IO.inspect(label: "Lower")
+    # upper |> IO.inspect(label: "Upper")
+
+    new_lower = if keep_lower == false, do: lower + Kernel.ceil((upper - lower) / 2), else: lower
+    new_upper = if keep_lower == true, do: upper - Kernel.ceil((upper - lower) / 2), else: upper
+
+    # new_lower |> IO.inspect(label: "New Lower")
+    # new_upper |> IO.inspect(label: "New Upper")
+    [new_lower, new_upper]
+  end
+
+  defp parse_row(row) do
+    [lower, _upper] = row |> Enum.reduce([0, 127], fn char, acc ->
+      acc |> new_range(char == "F")
+    end)
+
+    lower
+  end
+
+  defp parse_column(column) do
+    [_lower, upper] = column |> Enum.reduce([0, 7], fn char, acc ->
+      acc |> new_range(char == "L")
+    end)
+
+    upper
+  end
+
+  def part2(_input) do
   end
 end
