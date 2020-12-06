7e22548c9d48ef8cd8521a2843c804739a61017d
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-06 17:26:34 -0500

    Day 6 part 2 test complete
    Realized this is building on the earlier implementation from day 2 to count the number of characters in a string. I get the count of the enum to determine how many people should be present and inspect each character to see if the count matches. If it does, we add to the accumulator. The else statement here keeps us at zero instead of nil that it would default to.

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -12,6 +12,43 @@ defmodule AdventOfCode.Day06 do
     groups |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    _answers = input |> parse_answers2()
   end
+
+  defp parse_answers2(answers) do
+    # answers |> IO.inspect(label: "Answers")
+    groups = for answer <- answers do
+      answer |> passenger_count()
+    end
+    # groups |> IO.inspect(label: "Groups")
+    groups |> Enum.sum()
+  end
+
+  defp passenger_count(group) do
+    passengers = group |> Enum.count()
+    # passengers |> IO.inspect(label: "Passengers")
+    if passengers == 1 do
+      List.first(group) |> String.graphemes() |> Enum.count()
+    else
+      characters = group |> Enum.join() |> character_count()
+      # characters |> IO.inspect(label: "Characters")
+      _count = characters |> Enum.reduce(0, fn {_char, count}, acc ->
+        if count == passengers, do: acc + 1, else: acc
+      end)
+      # count |> IO.inspect(label: "Count")
+    end
+  end
+
+  defp character_count(input) do
+    input
+      |> String.graphemes()
+      |> Enum.reduce(%{}, fn char, acc ->
+           case acc do
+             %{^char => count} -> %{acc | char => count + 1}
+             _ -> Map.put(acc, char, 1)
+           end
+         end)
+  end
+
 end
