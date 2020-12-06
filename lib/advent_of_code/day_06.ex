be81d98813573adc0c3114429283a6475ac5f981
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-06 16:17:33 -0500

    Day 6 part 1 test complete
    Worked out the solution I figured would work. We join the list as a string, split it to graphemes (a list of individual letters), dedupe by using the uniq function then get the count of the individual letters. From here we get the sum to get the answer 11.

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,17 +1,15 @@
 defmodule AdventOfCode.Day06 do
   def part1(input) do
-    answers = input |> parse_answers
+    _answers = input |> parse_answers()
   end
 
   defp parse_answers(answers) do
-    answers |> IO.inspect(label: "Answers")
+    # answers |> IO.inspect(label: "Answers")
     groups = for answer <- answers do
-      values = for value <- answer do
-        value |> String.graphemes()
-      end
-      values |> IO.inspect(label: "Values")
+      answer |> Enum.join() |> String.graphemes() |> Enum.uniq() |> Enum.count()
     end
-    groups |> IO.inspect(label: "Groups")
+    # groups |> IO.inspect(label: "Groups")
+    groups |> Enum.sum()
   end
 
   def part2(_input) do
