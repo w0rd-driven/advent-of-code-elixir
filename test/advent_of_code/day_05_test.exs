c4c55e7eceece3865205296962803270d44b8c20
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-05 21:23:57 -0500

    Day 5 part 1 scaffold
    Add our starting point with input data. We're looking for the highest seat id, which given the 4 examples should be 820. Getting each boarding pass only involves reading the files line by line, which simplifies this a little bit.

--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -3,12 +3,10 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = get_input()
 
-    assert result
+    assert 820 = part1(input)
   end
 
   @tag :skip
@@ -17,5 +15,13 @@ defmodule AdventOfCode.Day05Test do
     result = part2(input)
 
     assert result
+  end
+
+  def get_input do
+    "../../lib/data/day_05_test.txt" # Eww but I don't care right now
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
+    |> Enum.to_list()
   end
 end
