3a211c9bc23f435926eba870279137a56af5f647
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-08 10:08:30 -0500

    Day 7 part 1 implementation halt
    Building a list of rules but from the hints I've looked for, this is a breadth first search vs a depth first search. One approach I was looking at was to loop over this result to combine all the counts and do a parent -> child -> child exponential count but I think that's wrong now because counts of parents will be different. Like if one bag has 4 and another has 3, making it 7 is naive and wrong depending on where in the graph it is. I'm going to jump to the next day and likely jump back to this with cheats. I want to keep the structure I built and even merging someone else's into that data structure is enough of a learning experience.

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,5 +1,39 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_input) do
+  def part1(input) do
+    count = for rule <- input do
+      rule |> parse_rule()
+    end
+
+  end
+
+  defp parse_rule(rule) do
+    rule |> IO.inspect(label: "Rule")
+    parts = rule |> String.split(" bags contain ")
+                 |> (fn [key, value] -> [{:bag, key}, {:contents, value}] end).()
+    # parts |> IO.inspect(label: "Parts")
+    bag = parts |> Keyword.get(:bag)
+    [primary, secondary] = bag |> String.split(" ")
+    color = "#{primary} #{secondary}"
+    # color |> IO.inspect(label: "Color")
+    contents = parts |> Keyword.get(:contents)
+    inventory = contents |> String.split(", ")
+    # inventory |> IO.inspect(label: "Inventory")
+    items = for item <- inventory do
+      case item |> String.split(" ") do
+        [count, item_primary, item_secondary, _] ->
+          %{:parent => color, :color => "#{item_primary} #{item_secondary}", :count => String.to_integer(count)}
+        [_no, _other, _bags] ->
+          %{:parent => color, :color => "no other", :count => 0}
+      end
+    end
+    items |> IO.inspect(label: "Items")
+    for %{:parent => parent, :color => color, :count => count} <- items, color != "shiny gold", reduce: 0 do
+      sum ->
+        parent |> IO.inspect(label: "Parent")
+        color |> IO.inspect(label: "Color")
+        count |> IO.inspect(label: "Count")
+        sum + 1
+    end
   end
 
   def part2(_input) do
