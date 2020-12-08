3edb23533f6fb5d3497bd1ca734a5fa373a51d69
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-08 15:26:56 -0500

    Day 8 part 1 complete
    Unpacked implementation at https://github.com/rkachowski/advent-of-code/blob/master/2020/8/solution.exs using case statement instead of function declarations. This is a bit of a cheat but I did work out that you needed a list of seen indexes as you jump around. Once you've been seen, you're back at an infinite loop. This example reinforces that Elixir types are immutable but to accumulate an object or result, passing it as an argument is a useful tactic. Impostor syndrome hit pretty hard for this tbh as my brain can't parse into concise functional concepts like this.

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,15 +1,40 @@
 defmodule AdventOfCode.Day08 do
   def part1(input) do
-    count = for instruction <- input do
+    instructions = for instruction <- input do
       instruction |> parse_instruction()
     end
+    # instructions |> IO.inspect(label: "Instructions")
+    {:loop, {_, acc, _}} = run_program(instructions, {0, 0, MapSet.new()})
+    acc
   end
 
   defp parse_instruction(instruction) do
-    instruction |> IO.inspect(label: "Instruction")
-    # parts = Regex.named_captures(~r/(?<instruction>nop|acc|jmp) (?<sign>\+|\-)(?<number>\d*)$/, instruction)
-    parts = Regex.run(~r/(\w+)\s((?:\+|-)\d+)/, instruction, capture: :all_but_first)
-    parts |> IO.inspect(label: "Parts")
+    # instruction |> IO.inspect(label: "Instruction")
+    _parts = Regex.run(~r/(\w+)\s((?:\+|-)\d+)/, instruction, capture: :all_but_first)
+    # parts |> IO.inspect(label: "Parts")
+  end
+
+  def run_program(instruction, {index, acc, seen}) when index >= length(instruction),
+    do: {:end, {index, acc, seen}}
+
+  def run_program(instruction, {index, acc, seen}) do
+    inst = Enum.at(instruction, index)
+    # inst |> IO.inspect(label: "Instruction")
+
+    cond do
+      MapSet.member?(seen, index) ->
+        {:loop, {index, acc, seen}}
+
+      true ->
+        case inst do
+          ["nop", _] ->
+            run_program(instruction, {index + 1, acc, MapSet.put(seen, index)})
+          ["acc", value] ->
+            run_program(instruction, {index + 1, acc + String.to_integer(value), MapSet.put(seen, index)})
+          ["jmp", value] ->
+            run_program(instruction, {index + String.to_integer(value), acc, MapSet.put(seen, index)})
+        end
+    end
   end
 
   def part2(_input) do
