c904ade7d30bfe1077a116439dc7f0e4ef295203
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-08 15:22:33 -0500

    Day 8 part 1 input parsing
    Parse implementation that returns a map with the instruction, sign, and number split out. Looked at https://github.com/rkachowski/advent-of-code/blob/master/2020/8/solution.exs and the regex is more concise.

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,5 +1,15 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_input) do
+  def part1(input) do
+    count = for instruction <- input do
+      instruction |> parse_instruction()
+    end
+  end
+
+  defp parse_instruction(instruction) do
+    instruction |> IO.inspect(label: "Instruction")
+    # parts = Regex.named_captures(~r/(?<instruction>nop|acc|jmp) (?<sign>\+|\-)(?<number>\d*)$/, instruction)
+    parts = Regex.run(~r/(\w+)\s((?:\+|-)\d+)/, instruction, capture: :all_but_first)
+    parts |> IO.inspect(label: "Parts")
   end
 
   def part2(_input) do
