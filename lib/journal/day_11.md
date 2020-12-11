0300953c5fe785a9c4dc18380d274c8ce5bd195b
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-11 12:13:39 -0500

    Day 11 part 1 scaffold
    Add puzzle and journal start. I remember doing this in a CS course but my mind is completely blank :>

---
+++ lib/journal/day_11.md
@@ -0,0 +1,5 @@
+# Journal Entry, December 11th 2020 (Friday)
+
+## Part 1
+
+## Part 2
--- test/advent_of_code/day_11_test.exs
+++ test/advent_of_code/day_11_test.exs
@@ -3,12 +3,10 @@ defmodule AdventOfCode.Day11Test do
 
   import AdventOfCode.Day11
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = get_input()
 
-    assert result
+    assert 37 = part1(input)
   end
 
   @tag :skip
@@ -17,5 +15,12 @@ defmodule AdventOfCode.Day11Test do
     result = part2(input)
 
     assert result
+  end
+
+  def get_input do
+    "../../lib/data/day_11_test.txt" # Eww but I don't care right now
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
   end
 end
