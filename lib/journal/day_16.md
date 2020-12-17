b2fd9a66ae0ba0552f33d921167f2b17322c9354
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2020-12-16 19:47:41 -0500

    Day 16 part 1 scaffold
    Add puzzle and journal starting point.

---
+++ lib/journal/day_16.md
@@ -0,0 +1,5 @@
+# Journal Entry, December 16th 2020 (Wednesday)
+
+## Part 1
+
+## Part 2
--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -3,12 +3,10 @@ defmodule AdventOfCode.Day16Test do
 
   import AdventOfCode.Day16
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = get_input()
 
-    assert result
+    assert 71 = part1(input)
   end
 
   @tag :skip
@@ -17,5 +15,12 @@ defmodule AdventOfCode.Day16Test do
     result = part2(input)
 
     assert result
+  end
+
+  def get_input do
+    "../../lib/data/day_16_test.txt" # Eww but I don't care right now
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
   end
 end
